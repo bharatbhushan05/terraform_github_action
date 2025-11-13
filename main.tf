5da277a00a495af16ce9a4e9fec4954c2cf0a2c8
Bharat Bhushan <111484672+bharatbhushan05@users.noreply.github.com>
2025-11-13 16:57:40 +0530

    formating issue resolved

--- main.tf
+++ main.tf
@@ -4,7 +4,6 @@ module "vpc" {
 
 module "security_group" {
   source = "./modules/security_group"
-  
-  # These are the ARGUMENT NAMES - they MUST match the security group module's variable names
-  vpc_id = module.vpc.vpc_id          
+
+  vpc_id = module.vpc.vpc_id
 }
